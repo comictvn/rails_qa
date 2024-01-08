b9ef8c7493c0776d3121812ac13e8f61d9cd3dc3
Casey (Jitera) <developers@jitera.com>
2024-01-08 07:21:57 +0000

    feat[service]: Complete user profile

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,7 +6,11 @@ class User < ApplicationRecord
   enum gender: %w[male female other], _suffix: true
 
   # validations
-
+  validates :age, numericality: { only_integer: true, greater_than_or_equal_to: 18, less_than_or_equal_to: 100, message: I18n.t('activerecord.errors.messages.not_an_integer') }
+  validates :gender, inclusion: { in: genders.keys, message: I18n.t('activerecord.errors.messages.in', count: genders.keys.join(', ')) }
+  validates :location, presence: true, length: { maximum: 255, message: I18n.t('activerecord.errors.messages.too_long', count: 255) }
+  validates :interests, length: { maximum: 1000, message: I18n.t('activerecord.errors.messages.too_long', count: 1000) }
+  validates :preferences, length: { maximum: 1000, message: I18n.t('activerecord.errors.messages.too_long', count: 1000) }
   # end for validations
 
   class << self
