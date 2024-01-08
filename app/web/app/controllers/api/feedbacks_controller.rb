6709b3d751b77235ff0e78ce2f210a6159d3e1d1
Jaimie (Jitera) <developers@jitera.com>
2024-01-08 16:43:51 +0000

    fix: 1 issues from deployment report

--- app/web/app/controllers/api/feedbacks_controller.rb
+++ app/web/app/controllers/api/feedbacks_controller.rb
@@ -3,11 +3,9 @@ class Api::FeedbacksController < ApplicationController
   before_action :doorkeeper_authorize!
 
   def create
-    feedback_service = FeedbackService::Create.new(
-      feedback_params[:user_id],
-      feedback_params[:match_id],
-      feedback_params[:comment]
-    )
+    feedback_service = FeedbackService::Create.new(*feedback_params.values_at(
+      :user_id, :matched_user_id, :comment
+    ))
     result = feedback_service.call
 
     case result[:status]
@@ -17,10 +15,10 @@ class Api::FeedbacksController < ApplicationController
       render json: { status: 422, message: result[:message] }, status: :unprocessable_entity
     end
   end
-
+  
   private
 
   def feedback_params
-    params.require(:feedback).permit(:user_id, :match_id, :matched_user_id, :comment)
+    params.require(:feedback).permit(:user_id, :matched_user_id, :comment)
   end
 end
